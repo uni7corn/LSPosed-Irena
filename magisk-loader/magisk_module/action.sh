37f1184c4815edb24a2131f98ba8622033e0ec3f
Irena <140869597+re-zero001@users.noreply.github.com>
2025-01-08 16:05:01 +0800

    Fix Action.sh

--- magisk-loader/magisk_module/action.sh
+++ magisk-loader/magisk_module/action.sh
@@ -1,1 +1,6 @@
-am broadcast -a "android.provider.Telephony.SECRET_CODE" -a "android.telephony.action.SECRET_CODE" -d "android_secret_code://5776733" "android"
+API=$(getprop ro.build.version.sdk)
+if [ "$API" -ge 29 ]; then
+  am broadcast -a android.telephony.action.SECRET_CODE -d android_secret_code://5776733 android
+else
+  am broadcast -a android.provider.Telephony.SECRET_CODE -d android_secret_code://5776733 android
+fi
